86db140d82df88eb66fac9aaae7c5408c331d2ff
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:01:54 +1300

    PATCH: module clean-up

--- ChildBlogEntries.php
+++ ChildBlogEntries.php
@@ -5,43 +5,44 @@
  *
  * @package blog
  */
-class ChildBlogEntries extends Widget {
+class ChildBlogEntries extends Widget
+{
+    public static $db = array(
+        'NumberOfItems' => 'Int'
+    );
+    public static $has_one = array();
 
-	static $db = array(
-		'NumberOfItems' => 'Int'
-	);
-	static $has_one = array();
+    public static $has_many = array();
 
-	static $has_many = array();
+    public static $many_many = array();
 
-	static $many_many = array();
+    public static $belongs_many_many = array();
 
-	static $belongs_many_many = array();
+    public static $defaults = array(
+        'NumberOfItems' => 7
+    );
 
-	static $defaults = array(
-		'NumberOfItems' => 7
-	);
+    public static $title = 'Child Blog Entries';
 
-	static $title = 'Child Blog Entries';
+    public static $cmsTitle = 'Child Blog Entries';
 
-	static $cmsTitle = 'Child Blog Entries';
+    public static $description = 'Show a list of latest child blog entries.';
 
-	static $description = 'Show a list of latest child blog entries.';
+    public function getCMSFields()
+    {
+        return new FieldSet(
+            new NumericField("NumberOfItems", "Number of items shown (will always show newest ones first)")
+        );
+    }
 
-	function getCMSFields() {
-		return new FieldSet(
-			new NumericField("NumberOfItems","Number of items shown (will always show newest ones first)")
-		);
-	}
-
-	function Links() {
-		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
-		$widgetAreaID = $this->ParentID;
-		$parent = DataObject::get_one("SiteTree", "NewsItemsID = ".$widgetAreaID);
-		if($parent) {
-			Requirements::themedCSS("widgets_ChildBlogEntries");
-			return DataObject::get("BlogEntry", "{$bt}ParentID{$bt} = ".$parent->ID, "{$bt}Created{$bt} DESC", null, $this->NumberOfItems);
-		}
-	}
-
+    public function Links()
+    {
+        $bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
+        $widgetAreaID = $this->ParentID;
+        $parent = DataObject::get_one("SiteTree", "NewsItemsID = ".$widgetAreaID);
+        if ($parent) {
+            Requirements::themedCSS("widgets_ChildBlogEntries");
+            return DataObject::get("BlogEntry", "{$bt}ParentID{$bt} = ".$parent->ID, "{$bt}Created{$bt} DESC", null, $this->NumberOfItems);
+        }
+    }
 }
